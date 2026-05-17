680a90e3bf90e2281aa04adae523066bb9992ecd
Your Name <you@example.com>
2026-05-17 22:17:37 +0000

    new try

--- graph.c
+++ graph.c
@@ -36,7 +36,7 @@ Graph* createGraph() {
 
 void addNode(Graph* g, const char* label) {
     if (!g || !label) return;
-    if(map_search(g->adjacencyMap, (*void)label) != NULL) {
+    if(map_search(g->adjacencyMap, (void*)label) != NULL) {
         return;
     }
     
